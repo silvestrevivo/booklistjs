a84b9b147393e1bcb765efe8bcc6a2db28015f1c
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-27 20:57:56 +0100

    adding new elements to dom with classes

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log('My Booklist App')
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI Class: Handle UI Tasks
+
+class UI {
+  // static funcions, why? This class contains the UI tasks and it will be not
+  // extended, thus its functions have to be 'isolated'
+  static displayBooks() {
+    // books stored in LocalStore
+    const storedBooks = [
+      {
+        title: 'title data',
+        author: 'author data',
+        isbn: 'isbn data'
+      }
+    ];
+    const books = storedBooks;
+    // As a map() in React...
+    books.forEach((book) => {
+      UI.addBookToList(book);
+    })
+  }
+
+  static addBookToList(book) {
+    const list = document.querySelector('#book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+    list.appendChild(row);
+  }
+}
+
+// Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks)
